4610363dc0e9594a5230cd6978190c9e4540eb7f
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2019-02-17 23:10:56 +0900

    Initial commit

---
+++ app/src/main/java/csd/srdg/sony/om/aws_amplifier/MyAdapter.java
@@ -0,0 +1,78 @@
+package csd.srdg.sony.om.aws_amplifier;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.amazonaws.amplify.generated.graphql.ListPetsQuery;
+import com.amazonaws.mobileconnectors.appsync.fetcher.AppSyncResponseFetchers;
+import com.apollographql.apollo.GraphQLCall;
+import com.apollographql.apollo.exception.ApolloException;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<ListPetsQuery.Item> mData = new ArrayList<>();;
+    private LayoutInflater mInflater;
+
+
+    // data is passed into the constructor
+    MyAdapter(Context context) {
+        this.mInflater = LayoutInflater.from(context);
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recycleview_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bindData(mData.get(position));
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    // resets the list with a new set of data
+    public void setItems(List<ListPetsQuery.Item> items) {
+        mData = items;
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txt_name;
+        TextView txt_description;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            txt_name = itemView.findViewById(R.id.txt_name);
+            txt_description = itemView.findViewById(R.id.txt_description);
+        }
+
+        void bindData(ListPetsQuery.Item item) {
+            txt_name.setText(item.name());
+            txt_description.setText(item.description());
+        }
+    }
+}
